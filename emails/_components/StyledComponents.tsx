e0a8fc53f749bc02920a6b7807529052edc1ed19
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-03-27 20:05:19 +0100

    fixed eslint issue

--- emails/_components/StyledComponents.tsx
+++ emails/_components/StyledComponents.tsx
@@ -1,4 +1,6 @@
-import { Text as UnstyledText, TextProps } from '@react-email/components';
+/* eslint-disable react/jsx-props-no-spreading */
+import type { TextProps } from '@react-email/components';
+import { Text as UnstyledText } from '@react-email/components';
 
 export const Text = (props: TextProps) => {
   const { ...forwardProps } = props;
