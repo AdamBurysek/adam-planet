bab96692d3837ed43ab4ff753013d10162022855
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-05-13 08:12:27 +0200

    fix - eslint import issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+import Footer from './_components/footer/Footer';
+import Navbar from './_components/navbar/Navbar';
+import Planet from './_components/planet/Planet';
 import About from './_sections/about/About';
 import Contact from './_sections/contact/Contact';
-import Footer from './_components/footer/Footer';
 import Gallery from './_sections/gallery/Gallery';
 import Hero from './_sections/hero/Hero';
 import Music from './_sections/music/Music';
-import Navbar from './_components/navbar/Navbar';
-import Planet from './_components/planet/Planet';
 import './globals.css';
 
 const App = () => (
